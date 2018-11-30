a29b724b54f9d48fd869ae25486f5a7e5a0d2e7d
Alexander Mazur <alexander.mazur@hotmail.com>
2018-11-30 12:00:03 -0800

    Correct descriptors

--- src/Build/DefaultAzureFactory.cs
+++ src/Build/DefaultAzureFactory.cs
@@ -9,15 +9,15 @@ namespace PipServices3.Azure.Build
 {
     public class DefaultAzureFactory : Factory
     {
-        public static Descriptor Descriptor = new Descriptor("pip-services", "factory", "azure", "default", "1.0");
+        public static Descriptor Descriptor = new Descriptor("pip-services3", "factory", "azure", "default", "1.0");
 
-        public static Descriptor StorageMessageQueueDescriptor = new Descriptor("pip-services", "message-queue", "storage-message-queue", "default", "1.0");
+        public static Descriptor StorageMessageQueueDescriptor = new Descriptor("pip-services3", "message-queue", "storage-message-queue", "default", "1.0");
         // TODO: Not ready for .net core 2.0, see  https://github.com/Azure/azure-service-bus-dotnet/issues/65
         //public static Descriptor ServiceBusMessageQueueDescriptor = new Descriptor("pip-services", "message-queue", "servicebus-message-queue", "*", "1.0");
         //public static Descriptor ServiceBusMessageTopicDescriptor = new Descriptor("pip-services", "message-queue", "servicebus-message-topic", "*", "1.0");
-        public static Descriptor KeyVaultConfigReaderDescriptor = new Descriptor("pip-services", "config-reader", "key-vault", "*", "1.0");
-        public static Descriptor AppInsightsCountersDescriptor = new Descriptor("pip-services", "counters", "app-insights", "*", "1.0");
-        public static Descriptor AppInsightsLoggerDescriptor = new Descriptor("pip-services", "logger", "app-insights", "*", "1.0");
+        public static Descriptor KeyVaultConfigReaderDescriptor = new Descriptor("pip-services3", "config-reader", "key-vault", "*", "1.0");
+        public static Descriptor AppInsightsCountersDescriptor = new Descriptor("pip-services3", "counters", "app-insights", "*", "1.0");
+        public static Descriptor AppInsightsLoggerDescriptor = new Descriptor("pip-services3", "logger", "app-insights", "*", "1.0");
 
         public DefaultAzureFactory()
         {
